2da21e397def4854bd723ca909ace3b2fbfc7b80
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-04-26 19:28:42 -0300

    fix createElement function

--- practicas/practica03/ejer04/js/helpers.js
+++ practicas/practica03/ejer04/js/helpers.js
@@ -2,28 +2,33 @@ const URL_EPISODES = 'https://rickandmortyapi.com/api/episode'
 
 /**
  * @param {keyof HTMLElementTagNameMap} tagName
- * @param {*} attributes
- * @param {HTMLElement} parent
+ * @param {object} attributes
+ * @param {HTMLElement} [parent]
  * @returns {HTMLElement}
  */
+export function createElement(tagName, attributes, parent) {
+  if (arguments.length === 3 && !(parent instanceof HTMLElement)) {
+    throw new Error('parent is not instance of HTMLElement')
+  }
 
-export function createElement(tagName, attributes = {}, parent) {
   const element = document.createElement(tagName)
   Object.keys(attributes).forEach((attr) => (element[attr] = attributes[attr]))
 
-  if (parent instanceof HTMLElement) parent.appendChild(element)
-  else if (parent !== undefined)
-    new Error('parent is not instance of HTMLElement')
-
+  if (arguments.length > 2) parent.appendChild(element)
   return element
 }
 
 export async function fetchEpisodes(page, name = '') {
   const resp = await fetch(`${URL_EPISODES}?page=${page}&name=${name}`)
+  if (resp.status !== 200) {
+    return {
+      pages: 0,
+      episodes: [],
+    }
+  }
   const data = await resp.json()
-
   return {
-    pages: data?.info?.pages ?? 0,
-    episodes: data?.results ?? [],
+    pages: data.info.pages,
+    episodes: data.results,
   }
 }
