5871bf51a5d3d6d79b057aad979ff5c521d6395c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-05-21 20:21:28 -0300

    add get /overweight_people

--- practicas/practica04/ejercicio07/index.js
+++ practicas/practica04/ejercicio07/index.js
@@ -8,13 +8,28 @@ const PORT = 3000
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
+
+function getImc(height, weight) {
+  return (parseInt(weight) / parseInt(height) ** 2) * 10000
+}
+
 app.get('/all', (req, res) => {
   res.json(JSON.parse(fs.readFileSync('./data/persons.json')))
 })
 
+/**
+ * Devuelve en formato JSON un arreglo con los nombres de las personas con un
+ * IMC mayor a 25
+ */
 app.get('/overweight_people', (req, res) => {
-  
+  const persons = JSON.parse(fs.readFileSync('./data/persons.json'))
+  const arr = []
+  persons.forEach(({ name, height, weight }) => {
+    if (getImc(height, weight) > 25) arr.push({ name })
+  })
+  res.json(arr)
 })
+
 
 app.listen(PORT, () => {
   console.log('\n', `Server listening on port http://localhost:${PORT}`, '\n\n')
