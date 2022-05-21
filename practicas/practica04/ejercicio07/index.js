0e10bcc3dadbd2f4000039088402665c24ebc9d0
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-05-21 20:46:53 -0300

    add get/people_by_age

--- practicas/practica04/ejercicio07/index.js
+++ practicas/practica04/ejercicio07/index.js
@@ -13,6 +13,15 @@ function getImc(height, weight) {
   return (parseInt(weight) / parseInt(height) ** 2) * 10000
 }
 
+function getAge(dob) {
+  const MIN_TO_MS = 60000
+  const dateDiff = new Date(Date.now() - dob.getTime())
+  const diffOffset = dateDiff.getTimezoneOffset() * MIN_TO_MS
+  const gmtDiff = new Date(dateDiff.getTime() + diffOffset)
+
+  return gmtDiff.getFullYear() - 1970
+}
+
 app.get('/all', (req, res) => {
   res.json(JSON.parse(fs.readFileSync('./data/persons.json')))
 })
@@ -30,6 +39,14 @@ app.get('/overweight_people', (req, res) => {
   res.json(arr)
 })
 
+/**
+ * Devuelve en formato JSON un arreglo de las edades de las personas indexado
+ * por el nombre de cada una
+ */
+app.get('/people_by_age', (req, res) => {
+  const persons = JSON.parse(fs.readFileSync('./data/persons.json'))
+  res.json(persons.map(({name, dob}) => ({[name]: getAge(new Date(dob))})))
+})
 
 app.listen(PORT, () => {
   console.log('\n', `Server listening on port http://localhost:${PORT}`, '\n\n')
