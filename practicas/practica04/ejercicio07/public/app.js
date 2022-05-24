0f7f12088a7e2098dda40f4081a6f6967963e912
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-05-24 18:47:22 -0300

    remove comments and btns ids on public/

--- practicas/practica04/ejercicio07/public/app.js
+++ practicas/practica04/ejercicio07/public/app.js
@@ -6,23 +6,6 @@ function renderData(query, textarea) {
     .then((data) => (textarea.innerText = JSON.stringify(data, null, 2)))
 }
 
-/*
-const btnOverweight = document.getElementById('overweight')
-const btnAge = document.getElementById('age')
-const btnImcOver40 = document.getElementById('imc-over-40')
-const btnAverage = document.getElementById('average')
-const btnYoungest = document.getElementById('youngest')
-const btnHeight = document.getElementById('height')
-
-btnOverweight.onclick = () => renderData('overweight_people', textarea)
-btnAge.onclick = () => renderData('people_by_age', textarea)
-btnImcOver40.onclick = () => renderData('imc_over_40', textarea)
-btnAverage.onclick = () => renderData('average_imc', textarea)
-btnYoungest.onclick = () => renderData('youngest', textarea)
-btnHeight.onclick = () => renderData('people_by_height', textarea)
-*/
-
 document.querySelectorAll('button').forEach((button) => {
   button.onclick = () => renderData(button.dataset.query, pre)
 })
-
